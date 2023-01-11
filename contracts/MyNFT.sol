7694c3436d9dfe04439fd8fdc3e8a29b21ea8f41
mayukoooo <yamagishi.work3@gmail.com>
2023-01-12 00:25:47 +0900

    feat: openzeppelin ERC721 import

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.14;
 
-contract MyNFT {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyNFT is ERC721 {
+    constructor() ERC721("MyNFT", "MYNFT"){}
     function nftMint() public {}
 }
