ec6940f662d56e7ace791401a32b70c8297e0883
mayukoooo <yamagishi.work3@gmail.com>
2023-01-15 20:46:42 +0900

    feat: nftMint作成

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -6,5 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNFT is ERC721 {
     constructor() ERC721("MyNFT", "MYNFT"){}
-    function nftMint() public {}
+
+    // NFT作成
+    function nftMint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
+    }
 }
